9802bc88f48e6c73722c3effe470009b3168104f
lzk <lzk@u-call.net>
2020-12-24 14:55:08 +0800

    编译和引用修正

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-var path = require('path');
-var nodeAbi = require('node-abi');
-var binary = require('node-pre-gyp');
-var fs = require('fs');
-
-var binding_path;
-if(fs.existsSync(path.join(__dirname, 'package.json'))) {
-  binding_path = binary.find(path.resolve(path.join(__dirname, 'package.json')));
-} else {
-  binding_path = path.join(__dirname, "lib", "binding", "node-v" + nodeAbi.getAbi(process.version) + "-" + process.platform + "-" + process.arch, "desktopIdle.node");
-}
-
-var desktopIdle = require(binding_path);
+var desktopIdle = require('./desktopIdle');
 module.exports = desktopIdle;
